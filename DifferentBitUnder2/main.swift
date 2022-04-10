99ba8aef8e40bfa656e554d96835102f58098e13
pow2267 <pow2267@gmail.com>
2022-04-10 22:00:09 +0900

    [Programmers] Find the smallest number which has one or two different bits from original number

--- DifferentBitUnder2/main.swift
+++ DifferentBitUnder2/main.swift
@@ -11,34 +11,64 @@ func solution(_ numbers: [Int64]) -> [Int64] {
     var result: [Int64] = []
     
     numbers.forEach{ number in
-        var loop: Bool = true
-        var plus: Int64 = 0
-                    
-        while loop {
-            plus += 1
-            var num = number
-            var newNum = number + plus
-            var diffCount = 0
+        var bits = convertInt64ToBits(number)
+        
+        // number가 짝수일 때
+        if number % Int64(2) == 0 {
+            bits[0] = 1
+        // number가 홀수일 때
+        } else {
+            var index = 0
             
-            repeat {
-                if (Int(num % Int64(2))) != (Int(newNum % Int64(2))) {
-                    diffCount += 1
+            while index < bits.count {
+                if bits[index] == 0 {
+                    bits[index] = 1
+                    bits[index-1] = 0
+                    break
+                } else {
+                    index += 1
                 }
-                
-                num = (num / Int64(2))
-                newNum = (newNum / Int64(2))
-            } while (num != 0 && diffCount < 3)
-            
-            if (Int(num % Int64(2))) != (Int(newNum % Int64(2))) {
-                diffCount += 1
             }
             
-            if diffCount < 3 {
-                result.append(number + plus)
-                loop = false
-            } else {
-                loop = true
+            // bits에 0이 없을 때
+            if index == bits.count {
+                bits[index-1] = 0
+                bits.append(1)
             }
+        }
+                    
+        result.append(convertBitsToInt64(bits))
+    }
+    
+    return result
+}
+
+func convertInt64ToBits(_ number: Int64) -> [Int] {
+    var bits: [Int] = []
+    var num = number
+    
+    repeat {
+        bits.append(Int(num % Int64(2)))
+        num = (num / Int64(2))
+    } while num != 0
+    
+    return bits
+}
+
+func convertBitsToInt64(_ bits: [Int]) -> Int64 {
+    var result: Int64 = 0
+    
+    for (index, value) in bits.enumerated() {
+        if index == 0 {
+            result += Int64(value)
+        } else {
+            var multi: Int64 = 1
+            
+            for _ in 0..<index {
+                multi *= Int64(2)
+            }
+            
+            result += (Int64(value) * multi)
         }
     }
     
